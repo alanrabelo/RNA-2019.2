1f2dd448fb0a0fdd3b89803d73a57e35b1ae3066
alan <alanrabelo13@gmail.com>
2019-10-23 12:34:52 -0300

    Final MLP

---
+++ MLP-Keras.py
@@ -0,0 +1,80 @@
+from data_manager import *
+from Models.MLP import *
+from Dataset.datasets import ClassificationDatasets, RegressionDatasets
+import pickle as pkl
+import tensorflow as tf
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.layers.core import Dense, Activation, Dropout
+from keras import backend as bk
+import pandas as pd
+import numpy as np
+
+datasets = [ClassificationDatasets.ARTIFICIAL_XOR,
+ClassificationDatasets.IRIS,
+ClassificationDatasets.DERMATOLOGY,
+ClassificationDatasets.CANCER,
+ClassificationDatasets.COLUNA]
+
+NUMBER_OF_FOLDS = 5
+NUMBER_OF_REALIZATIONS = 20
+NUMBER_OF_EPOCHS = 500
+NEURONS_RANGE = [3, 5, 7, 9, 12, 15]
+
+for dataset in datasets:
+
+    print('Treinando para o %s' % dataset.value)
+    validation_accuracy = []
+    best_neurons = []
+    number_of_hidden_neurons = NEURONS_RANGE
+    manager = DataManager(dataset)
+    x_TRAINNING, y_TRAINNING, x_VALIDATION, y_VALIDATION = manager.split_train_test_5fold(manager.X, manager.Y, hasvalidation=False)
+    for index in range(0, NUMBER_OF_REALIZATIONS):
+
+        results = {}
+
+        for number_of_neurons in number_of_hidden_neurons:
+
+            x_TRAINS, y_TRAINS, x_TESTS, y_TESTS = manager.split_train_test_5fold(x_TRAINNING, y_TRAINNING, hasvalidation=True)
+            accuracies = []
+
+            for i in range(0, NUMBER_OF_FOLDS):
+
+                model = Sequential()
+                model.add(Dense(number_of_neurons, input_dim=len(x_TRAINS[0][0])))
+                model.add(Activation('sigmoid'))
+                model.add(Dense(len(y_TRAINS[0][0])))
+                model.add(Activation('softmax'))
+                model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+                model.fit(x_TRAINS[i], y_TRAINS[i], epochs=200, verbose=0)
+                result = model.evaluate(x_TESTS[i], y_TESTS[i])
+                # correctness = perceptron.evaluate(x_TESTS[i], y_TESTS[i], should_print_confusion_matrix=True if index == 0 and i == 0 else False)
+                accuracies.append(result[1])
+                bk.clear_session()
+
+                # if index == 0 and i == 0:
+                #     perceptron.plot_decision_surface(x_TRAINS[i], y_TRAINS[i], x_TESTS[i], y_TESTS[i], 'MLP - '+dataset.value)
+
+            results[str(number_of_neurons)] = np.average(accuracies)
+
+        best_combination = sorted(results.items(), key=lambda x: x[0], reverse=False)[0]
+        model = Sequential()
+        model.add(Dense(int(best_combination[0]), input_dim=len(x_TRAINNING[0])))
+        model.add(Activation('sigmoid'))
+        model.add(Dense(len(y_TRAINNING[0])))
+        model.add(Activation('softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        model.fit(x_TRAINNING, y_TRAINNING, epochs=200)
+
+        result = model.evaluate(x_VALIDATION, y_VALIDATION)[1]
+        bk.clear_session()
+
+        validation_accuracy.append(result)
+        best_neurons.append(int(best_combination[0]))
+
+    print('Result for ' + dataset.value + ': ' + str(np.average(validation_accuracy) * 100))
+    print('STD was ' + dataset.value + ': ' + str(np.std(validation_accuracy)))
+    counts = np.bincount(best_neurons)
+    print('The best number of neurons was ' + str(np.argmax(counts)))
--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,7 @@
 import random
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+from Dataset.datasets import RegressionDatasets, ClassificationDatasets
 
 class DataManager:
 
@@ -8,15 +9,15 @@ class DataManager:
 
         self.validations_percentage = validations_percentage
         self.test_percentage = test_percentage
-        self.filename = filename.value
+        self.filename = filename
         self.X = []
         self.Y = []
         self.load_data()
 
 
-    def load_data(self):
+    def load_data(self, categorical=True):
 
-        with open('Dataset/' + self.filename, 'r') as file:
+        with open('Dataset/' + self.filename.value, 'r') as file:
             self.X = []
             self.Y = []
             categorical = {}
@@ -27,8 +28,11 @@ class DataManager:
                 input_formatted = []
 
                 line = line.replace('\n', '')
-                output = line.split(' ' if self.filename == 'column_3C.data' else ',')[-1]
-                input = line.split(' ' if self.filename == 'column_3C.data' else ',')[:-1]
+                split_line = line.split(' ' if self.filename == ClassificationDatasets.COLUNA else ',')
+
+                output = split_line[-1]
+                input = split_line[:-1]
+
 
                 for value in input:
                     try:
@@ -45,12 +49,14 @@ class DataManager:
                     categorical[output] = len(categorical.keys())
                     self.Y.append(categorical[output])
 
-            if self.filename == 'breast-cancer-wisconsin.data':
+            if self.filename == ClassificationDatasets.CANCER:
                 self.X = list(np.array(self.X)[:, 1:])
             scaler = MinMaxScaler(feature_range=(0, 1))
             x = scaler.fit_transform(self.X)
             self.X = x
-            self.Y = self.one_hot_encoding(self.Y)
+
+            if categorical:
+                self.Y = self.one_hot_encoding(self.Y)
 
     def one_hot_encoding(self, y_labels):
 
@@ -90,10 +96,10 @@ class DataManager:
             size_of_the_fold = round(len(X_shuffled)/5.0)
             split_point1 = i * size_of_the_fold
             split_point2 = split_point1 + size_of_the_fold
-            x_TRAINS.append(X_shuffled[:split_point1] + X_shuffled[split_point2:])
-            y_TRAINS.append(Y_shuffled[:split_point1] + Y_shuffled[split_point2:])
-            x_TESTS.append(X_shuffled[split_point1:split_point2])
-            y_TESTS.append(Y_shuffled[split_point1:split_point2])
+            x_TRAINS.append(np.array(X_shuffled[:split_point1] + X_shuffled[split_point2:]))
+            y_TRAINS.append(np.array(Y_shuffled[:split_point1] + Y_shuffled[split_point2:]))
+            x_TESTS.append(np.array(X_shuffled[split_point1:split_point2]))
+            y_TESTS.append(np.array(Y_shuffled[split_point1:split_point2]))
 
         if not hasvalidation:
             return np.array(x_TRAINS)[0], np.array(y_TRAINS)[0], np.array(x_TESTS)[0], np.array(y_TESTS)[0]
